ca458ed4cf583c200b9aa5a0c3b50b084646fc15
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2025-11-20 12:52:20 +0800

    Update extracted IP keys

--- get-github-whitelist.sh
+++ get-github-whitelist.sh
@@ -8,7 +8,7 @@
 OUTPUT_FILE="/etc/nginx/conf.d/whitelist/github-ip-list.whitelist"
 
 API_ENDPOINT="https://api.github.com/meta"
-IP_KEYS=".hooks[], .web[], .api[], .git[], .github_enterprise_importer[], .packages[], .pages[], .importer[], .actions[], .actions_macos[], .codespaces[], .dependabot[], .copilot[]"
+IP_KEYS=".hooks[], .web[], .api[], .git[], .github_enterprise_importer[], .packages[], .pages[], .importer[], .actions[], .actions_macos[], .codespaces[], .copilot[]"
 
 
 # Fetch IP addresses from GitHub's API directly and extract them
@@ -21,9 +21,12 @@ if [ $? -ne 0 ] || [ -z "$api_response" ]; then
 fi
 
 # Extract IP addresses from the response and append to the configuration file
+ip_list=$(jq -r "$IP_KEYS" <<< "$api_response" | sort -u)
+ip_count=$(echo "$ip_list" | wc -l)
+
 {
     echo "# GitHub services IP addresses"
-    jq -r "$IP_KEYS" <<< "$api_response" | sort -u | while read -r ip_address; do
+    echo "$ip_list" | while read -r ip_address; do
         echo "$ip_address  1;"
     done
 } > "$OUTPUT_FILE" || {
@@ -32,6 +35,7 @@ fi
 }
 
 
+echo "Total IP addresses: $ip_count"
 echo "GitHub IP address list has been updated in $OUTPUT_FILE"
 
 
